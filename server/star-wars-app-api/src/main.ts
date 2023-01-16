6076424c2215b872cb3b8bb37323091c93ee305e
idnbso <idanbusso@gmail.com>
2023-01-16 10:55:53 +0200

    Add api swagger support

--- server/star-wars-app-api/src/main.ts
+++ server/star-wars-app-api/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Star Wars App Api')
+    .setDescription(
+      'This Api fetches data regarding Star Wars movies from swapi.dev',
+    )
+    .setVersion('1.0')
+    .addTag('star-wars')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   await app.listen(3000);
 }
